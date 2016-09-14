854c6625d5080b93cc837401593dece8d6ceba68
Jesse Nayak <jdn4ae@virginia.edu>
2016-09-14 19:13:57 -0400

    Add comments

--- mysite/mysite/settings/local-dist.py
+++ mysite/mysite/settings/local-dist.py
@@ -1,5 +1,6 @@
 """Copy this file into local.py and fill in blanks"""
-# Do not fill these fields out in local-dist.py or defaults.py
+
+# Do NOT fill these fields out in local-dist.py or defaults.py
 
 
 # Any settings that contain keys should be left blank in
@@ -8,7 +9,8 @@
 # This is to avoid putting private keys on Github for
 # everyone to see
 
-SECRET_KEY = '' 
+SECRET_KEY = ''  #  fill this out with a long random string
+                 #  in local.py
 
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
