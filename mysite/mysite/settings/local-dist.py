d6b71783728e127330a767eb57da95288de6855c
Jesse Nayak <jdn4ae@virginia.edu>
2016-09-14 20:23:21 -0400

    Add admin, requirement and comments

--- mysite/mysite/settings/local-dist.py
+++ mysite/mysite/settings/local-dist.py
@@ -15,8 +15,17 @@ SECRET_KEY = ''  #  fill this out with a long random string
 
 # django-storages
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+
+# Log on to aws, go to IAM console
+# Create a user or go to your user if you alread have one
+# Click "Create access key"
+# Copy keys here
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
+
+# Log on to aws, go to s3 console
+# copy name of bucket in the list here
+# it should look like elasticbeanstalk-us-east-1-...
 AWS_STORAGE_BUCKET_NAME = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
