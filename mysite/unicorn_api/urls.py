c5d39054cdcf0983186d5c3f7196652f510b0276
Jesse Nayak <jdn4ae@virginia.edu>
2016-09-17 15:53:22 -0400

    Restrict HTTP methods on endpoints to just get

--- mysite/unicorn_api/urls.py
+++ mysite/unicorn_api/urls.py
@@ -3,46 +3,34 @@ from unicorn import views
 
 author_list = views.AuthorViewSet.as_view({
     'get': 'list',
-    'post': 'create',
 })
 
 author_detail = views.AuthorViewSet.as_view({
     'get': 'retrieve',
-    'delete': 'destroy',
-    'patch': 'partial_update'
 })
 
 article_list = views.ArticleViewSet.as_view({
     'get': 'list',
-    'post': 'create',
 })
 
 article_detail = views.ArticleViewSet.as_view({
     'get': 'retrieve',
-    'delete': 'destroy',
-    'patch': 'partial_update'
 })
 
 tag_list = views.TagViewSet.as_view({
     'get': 'list',
-    'post': 'create',
 })
 
 tag_detail = views.TagViewSet.as_view({
     'get': 'retrieve',
-    'delete': 'destroy',
-    'patch': 'partial_update'
 })
 
 article_image_list = views.ArticleImageViewSet.as_view({
     'get': 'list',
-    'post': 'create',
 })
 
 article_image_detail = views.ArticleImageViewSet.as_view({
     'get': 'retrieve',
-    'delete': 'destroy',
-    'patch': 'partial_update'
 })
 
 urlpatterns = [
