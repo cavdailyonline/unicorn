d46e361f8ec1219238cb10a362a4e41052324665
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-09-17 22:39:05 -0400

    adding generic views across the board

--- mysite/unicorn/urls.py
+++ mysite/unicorn/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.articleList, name='Article List'),
-    url(r'^dash$', views.dashboard, name='dashboard'),
-    url(r'^(?P<slug>[\w-]+)/$', views.articleDetail, name='detail'),
+    url(r'^articles/$', views.ArticleList.as_view(), name='Article List'),
+    url(r'^articles/(?P<slug>[-\w]+)/$', views.ArticleDetail.as_view(), name= 'Article Detail'),
+    url(r'^tags/$', views.TagList.as_view(), name='Tag List'),
+    url(r'^tags/(?P<text>[-\w]+)/$', views.TagDetail.as_view(), name= 'Tag Detail'),
+    url(r'^authors/$', views.AuthorList.as_view(), name='Author List'),
 ]
