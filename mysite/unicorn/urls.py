1c0b10bb78cd35ceabe698a7416a6d33ac1e0c28
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-09-17 22:53:38 -0400

    pep8 fixes

--- mysite/unicorn/urls.py
+++ mysite/unicorn/urls.py
@@ -3,9 +3,13 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^articles/$', login_required(views.ArticleList.as_view()), name='Article List'),
-    url(r'^articles/(?P<slug>[-\w]+)/$', login_required(views.ArticleDetail.as_view()), name= 'Article Detail'),
+    url(r'^articles/$', login_required(views.ArticleList.as_view()),
+        name='Article List'),
+    url(r'^articles/(?P<slug>[-\w]+)/$',
+        login_required(views.ArticleDetail.as_view()), name='Article Detail'),
     url(r'^tags/$', login_required(views.TagList.as_view()), name='Tag List'),
-    url(r'^tags/(?P<text>[-\w]+)/$', login_required(views.TagDetail.as_view()), name= 'Tag Detail'),
-    url(r'^authors/$', login_required(views.AuthorList.as_view()), name='Author List'),
+    url(r'^tags/(?P<text>[-\w]+)/$',
+        login_required(views.TagDetail.as_view()), name='Tag Detail'),
+    url(r'^authors/$', login_required(views.AuthorList.as_view()),
+        name='Author List'),
 ]
