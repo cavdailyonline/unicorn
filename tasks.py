75994f69087885abf13d2b964bf392c953c89c1c
Jesse Nayak <jdn4ae@virginia.edu>
2016-09-18 16:47:28 -0400

    Clean up tasks.py

--- tasks.py
+++ tasks.py
@@ -2,13 +2,13 @@ import os
 from invoke import task
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-API = os.path.join(HERE, 'mysite')
+APP = os.path.join(HERE, 'mysite')
 
 
 @task
 def test_all(ctx):
     flake(ctx)
-    test_api(ctx)
+    test_app(ctx)
     # TODO: add more as they become available
 
 # PYTHON
@@ -18,21 +18,16 @@ def test_all(ctx):
 def flake(ctx, echo=True):
     # Ignore too long lines with E501 because
     # it throws and error on every migration file
-    ctx.run('flake8 {} {}'.format(API,
+    ctx.run('flake8 {} {}'.format(APP,
                                   "--ignore=E501"), echo=echo)
 
 
 @task
-def api_server(ctx):
-    ctx.run('python {}/manage.py runserver'.format(API), echo=True)
+def app_server(ctx):
+    ctx.run('python {}/manage.py runserver'.format(APP), echo=True)
 
 
 @task
-def test_api(ctx):
+def test_app(ctx):
     ctx.run(
-        'python {}/manage.py test {}'.format(API, API), echo=True, pty=True)
-
-
-@task(aliases=['req'])
-def requirements(ctx):
-    ctx.run('pip install -r requirements.txt', echo=True)
+        'python {}/manage.py test {}'.format(APP, APP), echo=True, pty=True)
