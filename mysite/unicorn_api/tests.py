4fd19f295246bdb53e59e22542b02e0ee6960712
Jesse Nayak <jdn4ae@virginia.edu>
2016-09-18 11:20:59 -0400

    Add test stubs

--- mysite/unicorn_api/tests.py
+++ mysite/unicorn_api/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase  # noqa
+from rest_framework.test import APITestCase
+from unicorn.models import Author, Article, Tag, ArticleImage
 
 # Create your tests here.
+
+
+class GetAuthorTest(APITestCase):
+
+    def setUp():
+        pass
+
+    def test_can_get_author():
+        pass
+
+    def test_can_get_author_list():
+        pass
+
+
+class GetArticleTest(APITestCase):
+
+    def setUp():
+        pass
+
+    def test_can_get_article():
+        pass
+
+    def test_can_get_article_list():
+        pass
+
+
+class GetTagTest(APITestCase):
+
+    def setUp():
+        pass
+
+    def test_can_get_tag():
+        pass
+
+    def test_can_get_tag_list():
+        pass
+
+class GetArticleImageTest(APITestCase):
+
+    def setUp():
+        pass
+
+    def test_can_get_article_image():
+        pass
+
+    def test_can_get_article_image_list():
+        pass
