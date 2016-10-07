f5ef790149abe5d4e3c2c48d845c5ee3da86e1cd
Jesse Nayak <jdn4ae@virginia.edu>
2016-10-07 17:31:20 -0400

    Fix travis ci to account for refactor

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 
